481936b16a41405205baf6fb06668d01b07d04ff
Eduard0126 <mano0065@algonquinlive.com>
2022-08-31 01:17:38 -0400

    all updates

--- script.js
+++ script.js
@@ -1,17 +1,39 @@
 
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
+let roundNum = 0;
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Enter your choice',"rock, paper, scissors");
-        const computerSelection = getComputerChoice();
-        console.log('Player choice is: ' + playerSelection)
-        console.log('Computer choice is: ' + computerSelection)
-        console.log(playRound(playerSelection.toLowerCase(), computerSelection))
-    } if (playerScore > computerScore) {console.log("Final Result : You Win")
-    } else (console.log("Final Result : You lose"));
-    }
+
+
+// Naming User and Computer Scores to update
+const resultPlayer = document.querySelector('#playerScore');
+resultPlayer.textContent = "Player Score: " + playerScore;
+const resultComputer = document.querySelector('#computerScore');
+resultComputer.textContent ="Computer Score: " + computerScore;
+
+
+// Selecting the inapp message for the game
+const inApp = document.querySelector('#inApp')
+const finalResult = document.querySelector('.finalResult');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+
+
+
+// Selecting the player's choice
+let selection = document.querySelector('#buttons');
+selectionListen = selection.addEventListener('click', function(e) 
+    {roundNum++;
+        const playAgainBtn = document.querySelector('.btn');
+        playAgainBtn.addEventListener('click', restartGame);
+    if (roundNum === 5) {game();openModal();}; 
+    if (e.target.id === 'buttons') {return;}
+    playerSelection = e.target.id;
+    computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+});
+
+
 
 //Generating the random selection for computer
 function getComputerChoice () {
@@ -20,21 +42,53 @@ function getComputerChoice () {
     return randomARR
 }
 
+/// Play the round and display the result
+const result = document.querySelector('#result');
+const playerPlayed = document.querySelector('#playerPlayed');
+const computerPlayed = document.querySelector('#computerPlayed');
+
+
+
 function playRound (playerSelection, computerSelection) {
+    function played() {
+        playerPlayed.textContent = `You played : ${playerSelection}`;
+        computerPlayed.textContent = `Computer played : ${computerSelection}`;}
+    
 if (playerSelection === computerSelection) {
-    console.log("Buuu! It's a tie, let's try one more time"); playerScore++}
+    result.textContent = "Hey! It's a tie, let's try one more time";played();}
+
 else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-    console.log("You Win! Rock beats Scissors"); playerScore++}
+    result.textContent = "You Win! Rock beats Scissors"; playerScore++;
+    resultPlayer.textContent = "Player Score: " + playerScore;played();}
+
 else if (playerSelection === 'paper' && computerSelection === 'rock') {
-    console.log("You Win! Paper beats Rock"); playerScore++}
+    result.textContent ="You Win! Paper beats Rock"; playerScore++;
+    resultPlayer.textContent = "Player Score: " + playerScore;played();}
+
 else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-    console.log("You Win! Scissors beats Paper"); playerScore++}
+    result.textContent ="You Win! Scissors beats Paper"; playerScore++;
+    resultPlayer.textContent = "Player Score: " + playerScore;played();}
+
 else {
-     console.log(`You lose! ${computerSelection} beats ${playerSelection}`); computerScore++}
+     result.textContent = `You lose! ${computerSelection} beats ${playerSelection}`; computerScore++;
+     resultComputer.textContent ="Computer Score: " + computerScore;played();}
 }
 
 
-console.log(game())
 
 
+function game() {
+    if (playerScore > computerScore) {finalResult.textContent = "You Win 😄";} 
+    else if (playerScore === computerScore) {finalResult.textContent = "It's a tie 😬";}
+    else if  (playerScore < computerScore) {finalResult.textContent = "You Lose 🥺";};
+}
 
+function openModal() {
+    overlay.style.display = "block";
+    modal.style.display = "block";
+  }
+
+
+function restartGame() {
+    window.location.reload(); 
+}
